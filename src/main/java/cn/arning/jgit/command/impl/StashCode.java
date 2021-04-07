6366badf39d84cf81c6f5cb7d7544102a0477dc7
gxn <gxn0627@gmail.com>
2021-04-07 20:58:18 +0800

    暂存类

---
+++ src/main/java/cn/arning/jgit/command/impl/StashCode.java
@@ -0,0 +1,14 @@
+package cn.arning.jgit.command.impl;
+
+import cn.arning.jgit.command.Execute;
+import org.eclipse.jgit.api.Git;
+
+/**
+ * @author arning
+ */
+public class StashCode implements Execute {
+    @Override
+    public void execute(Git git, String describe, String version) {
+        
+    }
+}
