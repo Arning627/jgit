07acc5a257d8aa82650ab7315dbc56ec655f1258
gxn <gxn0627@gmail.com>
2021-04-07 20:56:51 +0800

    简单实现提交

--- src/main/java/cn/arning/jgit/command/impl/PushCode.java
+++ src/main/java/cn/arning/jgit/command/impl/PushCode.java
@@ -1,7 +1,12 @@
 package cn.arning.jgit.command.impl;
 
 import cn.arning.jgit.command.Execute;
+import cn.arning.jgit.conf.GitAuthentication;
+import org.eclipse.jgit.api.DiffCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.diff.DiffEntry;
+
+import java.util.List;
 
 
 /**
@@ -12,6 +17,22 @@ public class PushCode implements Execute {
 
     @Override
     public void execute(Git git, String describe, String version) {
+        try {
+            List<DiffEntry> call = git.diff().call();
+            for (DiffEntry diffEntry : call) {
+                String diffEntryNewPath = diffEntry.getNewPath();
+                if (diffEntryNewPath.startsWith(".")){
+                    continue;
+                }
+                git.add().addFilepattern(diffEntryNewPath).call();
+            }
+            git.commit().setMessage(describe).call();
+            git.push().setRemote("origin").setCredentialsProvider(GitAuthentication.authentication()).call();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
 
     }
 }
--- src/test/java/cn/arning/jgit/JgitApplicationTests.java
+++ src/test/java/cn/arning/jgit/JgitApplicationTests.java
@@ -5,6 +5,9 @@ import cn.arning.jgit.utils.FileUtil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheBuilder;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.RemoteConfig;
@@ -20,12 +23,12 @@ import java.util.List;
 class JgitApplicationTests {
 
 
-
+    String path = "/Users/arning/Desktop/tmp/gitTest/.git";
 
     @Test
     void contextLoads() {
         File file = new File("/Users/arning/develop/code/my");
-        List<File> localGitRepository = FileUtil.findLocalGitRepository(file,new ArrayList<File>());
+        List<File> localGitRepository = FileUtil.findLocalGitRepository(file, new ArrayList<File>());
         for (File file1 : localGitRepository) {
             String absolutePath = file1.getAbsolutePath();
             System.out.println(absolutePath);
@@ -37,13 +40,29 @@ class JgitApplicationTests {
 
     @Test
     void test1() throws IOException, GitAPIException {
-        File file = new File("/Users/arning/Desktop/tmp/gitTest/.git");
-        Git git  = Git.open(file);
+        File file = new File(path);
+        Git git = Git.open(file);
         String branch = git.getRepository().getBranch();
         System.out.println(branch);
-
     }
 
+    @Test
+    void test2() throws Exception{
+        File file = new File(path);
+        Git open = Git.open(file);
+        List<DiffEntry> call = open.diff().call();
+        for (DiffEntry diffEntry : call) {
+            String newPath = diffEntry.getNewPath();
+            if (newPath.startsWith(".")){
+                continue;
+            }
+            DirCache add = open.add().addFilepattern(newPath).call();
+        }
+        open.commit().setMessage("测试提交").call();
+        open.push().setRemote("origin").setCredentialsProvider(GitAuthentication.authentication()).call();
+
+
+    }
 
 
 }
