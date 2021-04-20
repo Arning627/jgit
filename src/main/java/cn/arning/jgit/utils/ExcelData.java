bdd7bb73b901b365009db388a2a6c7251ac0d8c5
gxn <gxn0627@gmail.com>
2021-04-20 10:15:33 +0800

    加插件

--- src/main/java/cn/arning/jgit/utils/ExcelData.java
+++ src/main/java/cn/arning/jgit/utils/ExcelData.java
@@ -1,42 +1,20 @@
 package cn.arning.jgit.utils;
 
-import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.metadata.BaseRowModel;
+
+import lombok.Data;
+import lombok.ToString;
 
 /**
  * @author arning
  */
-public class ExcelData extends BaseRowModel {
+@Data
+@ToString
+public class ExcelData {
 
-    @ExcelProperty
     private String projectName;
 
     private String url;
 
 
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-
-    @Override
-    public String toString() {
-        return "ExcelData{" +
-                "projectName='" + projectName + '\'' +
-                ", url='" + url + '\'' +
-                '}';
-    }
 }
 
